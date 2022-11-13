9830f16c3e90551e5d10a481dbe7e7dcd3c8f080
Furkan Enes Eren <furkanenes_98_@hotmail.com>
2022-11-14 02:32:51 +0300

    listener functionality

--- TweetListener.py
+++ TweetListener.py
@@ -5,6 +5,7 @@ import socket
 import json
 from dotenv import load_dotenv
 import os
+import time
 
 
 def configure():
@@ -22,26 +23,34 @@ class MyStreamListener(tweepy.StreamingClient):
         print("Connected")
 
   def on_tweet(self, tweet):
-    # Displaying tweet in console
-    if tweet.referenced_tweets == None:
-        print(tweet.text)
+    try:
+        s = socket.socket()         # Create a socket object
+        host = "127.0.0.1"     # Get local machine name
+        port = 5554                 # Reserve a port for your service.
+        s.bind((host, port))        # Bind to the port
 
-  def on_data(self, data):
-      try:
-          msg = json.loads(data)
-          print(msg['text'].encode('utf-8'))
-          self.client_socket.send(msg['text'].encode('utf-8'))
-          return True
-      except BaseException as e:
+        print("Listening on port: %s" % str(port))
+
+        s.listen(5)                 # Now wait for client connection.
+        c, addr = s.accept()        # Establish connection with client.
+
+        print( "Received request from: " + str( addr ) )
+        msg = tweet.data
+        print(msg['text'].encode('utf-8'))
+        c.send((str(msg['text']) + "\n").encode('utf-8'))
+        time.sleep(0.5)
+        return True
+    except BaseException as e:
           print("Error on_data: %s" % str(e))
-      return True
+    return True
+
 
   def on_error(self, status):
       print(status)
       return True
 
 
-def sendData(c_socket):
+def sendData():
     client = tweepy.Client(os.getenv('bearer_token'), os.getenv('api_key'), os.getenv('api_key_secret'), os.getenv('access_token'), os.getenv('access_token_secret'))
     auth = tweepy.OAuth1UserHandler(os.getenv('api_key'), os.getenv('api_key_secret'), os.getenv('access_token'), os.getenv('access_token_secret'))
     api = tweepy.API(auth)
@@ -54,7 +63,7 @@ def sendData(c_socket):
         stream.add_rules(tweepy.StreamRule(term))
 
    
-    stream.filter(tweet_fields=["referenced_tweets"])
+    stream.filter(tweet_fields=["text"])
 
     """myStreamListener = MyStreamListener()
     myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
@@ -63,16 +72,4 @@ def sendData(c_socket):
 
 if __name__ == "__main__":
     configure()
-    s = socket.socket()         # Create a socket object
-    host = "127.0.0.1"     # Get local machine name
-    port = 5554                 # Reserve a port for your service.
-    s.bind((host, port))        # Bind to the port
-
-    print("Listening on port: %s" % str(port))
-
-    s.listen(5)                 # Now wait for client connection.
-    c, addr = s.accept()        # Establish connection with client.
-
-    print( "Received request from: " + str( addr ) )
-
-    sendData( c )
+    sendData()
